f7e650d578963f0cbe70b8dd96119d68a2b98491
Rahul <115809712+R23-Rahul@users.noreply.github.com>
2024-07-11 22:43:54 +0530

    Update GraphCreation.cpp

--- GraphCreation.cpp
+++ GraphCreation.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 class Graph{
   public:
+//adjacency list for un-weighted graph
   unordered_map<int,list<int>>adjList;
+//adj list for weighted graph
   unordered_map<int,list<pair<int,int>>> WtadjList;
 
+//for un-weighted graph
   void addEdge(int u, int v , bool direction){
 
   //if direction==0 => undirected graph
@@ -19,7 +22,11 @@ class Graph{
   }
 
   }
+
+//for weighted graph
   void weightedEdgeAdd(int u,int v, int wt, bool direction){
+      //if direction==0 => undirected graph
+  //if direction==1=>directed Graph
     if(direction==1){
       WtadjList[u].push_back({v,wt});
     }
@@ -29,16 +36,17 @@ class Graph{
     }
 
   }
-
+//print adjacency list for weighted graph
   void printUnWeightedAdjList(){
    for(auto i : adjList){
      cout<<i.first<<" -> { ";
      for(auto nbr : i.second){
       cout<<nbr<<", ";
      }
-     cout<<endl;
+     cout<<" }"<<endl;
    }
   }
+//print the adjacency list for weighted graph
   void printWeightedAdjList(){
    for(auto i: WtadjList){
     cout<<i.first<<"->{ ";
@@ -53,10 +61,13 @@ class Graph{
 };
 
 int main() {
+  //create graph
 Graph g;
+  //insert  edges in graph
 g.weightedEdgeAdd(1,2,5,0);
 g.weightedEdgeAdd(2,3,8,0);
 g.weightedEdgeAdd(2,4,10,0);
+  //print adjlist
 g.printWeightedAdjList();
   return 0;
 }
